5b97c86e507e0c490475560c631922bd2f3719da
jmalhot <35250709+jmalhot@users.noreply.github.com>
2018-02-12 18:05:09 +0530

    Update demo.sql

--- AmexJenkins_Liquibase_scripts/demo.sql
+++ AmexJenkins_Liquibase_scripts/demo.sql
@@ -5,7 +5,7 @@
 --  DDL for Table DEMOJENKINS
 --------------------------------------------------------
 
-  CREATE TABLE "DEMOJENKINS" 
+  CREATE TABLE "DEMOJENKINSLIQUIBASE" 
    (	"ID" NUMBER(*,0), 
 	"TESTNAME" VARCHAR2(50 BYTE)
    )
