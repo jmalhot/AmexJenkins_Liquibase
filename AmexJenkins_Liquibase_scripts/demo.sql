7638b05a39a76a2034552db59bdd7fe3a9f56ecd
jmalhot <35250709+jmalhot@users.noreply.github.com>
2018-02-13 15:13:54 +0530

    Update demo.sql

--- AmexJenkins_Liquibase_scripts/demo.sql
+++ AmexJenkins_Liquibase_scripts/demo.sql
@@ -8,8 +8,6 @@
   CREATE TABLE "DEMOJENKINS" 
    (	"ID" NUMBER, 
 	"TESTNAME" VARCHAR2(50)
-   );
-   /\s*
-   |
-   /\s*$
-   INSERT INTO DEMOJENKINS(ID, TESTNAME) VALUES(2,'SecondTest');
+   )
+  /
+   INSERT INTO DEMOJENKINS(ID, TESTNAME) VALUES(2,'SecondTest')
